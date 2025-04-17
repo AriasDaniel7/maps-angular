2b9373ec4c3f7c72e3bd70579d5cf72542017974
AriasDaniel7 <daniel7arias2003@gmail.com>
2025-04-17 17:45:56 -0500

    Inicio del proyecto

---
+++ src/app/pages/houses-page/houses-page.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-houses-page',
+  imports: [],
+  templateUrl: './houses-page.component.html',
+  styleUrl: './houses-page.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HousesPageComponent {}
---
+++ src/app/pages/markers-page/markers-page.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-markers-page',
+  imports: [],
+  templateUrl: './markers-page.component.html',
+  styleUrl: './markers-page.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MarkersPageComponent {}
